7f4f89af66ae27d88aebaeac117ff8f56484f864
DeloxyAdeola <108836569+DeloxyAdeola@users.noreply.github.com>
2022-10-21 12:23:21 -0400

    Add files via upload

---
+++ crowdfunding_db_schema (2).sql
@@ -0,0 +1,41 @@
+Campaign
+-
+cf_id int Pk
+contact_id int FK - contacts.contact_id
+company_name varchar(100).
+description text.
+goal numeric(10,2)
+pledged numeric(10,2)
+outcome varchar(50)
+backers_count int
+country varchar(10)
+currency varchar(10)
+launch_date date.
+end_date date.
+category_id varchar(10) FK >- category.category_id
+subcategory_id varchar(10) FK >- subcategory.subcategory_id
+
+Category
+-
+category_id PK varchar(50).
+category_name varchar(50).
+
+subcategory
+-
+subcategory_id PK varchar(10).
+subcategory_name varchar(50).
+
+contacts
+-
+contact_id PK int
+first_name varchar(50).
+last_name varchar(50).
+email varchar(100).
+
+backers 
+-
+backer_id pk int
+cf_id vachar(10) pk fk - campaign.cf_id
+first_name varchar
+last_name varchar
+email varchar
